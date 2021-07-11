deab8ff558c012e209a501a79a8fc31520492788
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-11 11:17:51 +0530

    setting up backend

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hel</h1>');
+});
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
