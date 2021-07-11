93e08ab519954cffd84eee3641e313dd8cddb30d
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-11 13:15:28 +0530

    added schemas

---
+++ models/invoice.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const invoiceSchema = mongoose.Schema({
+  to: {
+    address: String,
+    city: String,
+    country: String,
+    postcode: Number,
+  },
+  client: {
+    name: String,
+    email: String,
+    address: String,
+    city: String,
+    postcode: Number,
+    country: String,
+    date: Date,
+    terms: String,
+    description: String,
+  },
+  items: [{ name: String, quantity: String, price: Number }],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+invoiceSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Invoice', invoiceSchema);
