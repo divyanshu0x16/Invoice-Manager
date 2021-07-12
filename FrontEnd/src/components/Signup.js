aa48e92a13a299a7b29b70e2f3ec8c32d3f4eb13
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-12 11:01:08 +0530

    added signup form

--- FrontEnd/src/components/Signup.js
+++ FrontEnd/src/components/Signup.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Signup = () => {
-    return (
-        <div>
-            This is the SingUp page
-        </div>
-    )
-}
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Signup
+  const handleSignup = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSignup}>
+        <div>
+          Username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          Name
+          <input
+            type="text"
+            value={name}
+            name="Name"
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          Password
+          <input
+            type="text"
+            value={password}
+            name="Name"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>SignUp</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
