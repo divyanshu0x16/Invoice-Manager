c27a83fda101e033c87a46aad5386b198bcfdc9d
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-12 23:33:17 +0530

    user logouts on token expiry

--- FrontEnd/src/components/Dashboard.js
+++ FrontEnd/src/components/Dashboard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const Dashboard = () => {
   const user = JSON.parse(localStorage.getItem('userDetails'));
@@ -7,6 +9,12 @@ const Dashboard = () => {
   if (user === null) {
     return <Redirect to="/login" />;
   }
+
+  jwt.verify(user.token, process.env.REACT_APP_SECRET, (err) => {
+    if (err) {
+      localStorage.clear();
+    }
+  });
 
   return <div>And This is the Dashboard</div>;
 };
