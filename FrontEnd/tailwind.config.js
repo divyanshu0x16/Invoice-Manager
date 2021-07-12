cc4fd0d5633227d9e52de870975849aab412e574
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-12 23:49:39 +0530

    added tailwind colors

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -1,5 +1,5 @@
-const colors = require('tailwindcss/colors')
-const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
@@ -7,8 +7,22 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Spartan', ...defaultTheme.fontFamily.sans]
+        sans: ['Spartan', ...defaultTheme.fontFamily.sans],
       },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.red,
+      yellow: colors.amber,
+      pink: colors.pink,
+      purple: colors.purple,
+      blue: colors.blue,
+      green: colors.emerald,
     },
   },
   variants: {
