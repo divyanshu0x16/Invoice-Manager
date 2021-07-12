f13dd678ff4ca8f26fd199f4fbd24527600615c2
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-12 22:51:31 +0530

    initialized navbar

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Dashboard} />
         <Route path="/login" component={Login} />
