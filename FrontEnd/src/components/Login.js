f5ce3ae3d3b17a990166cd436f0c019a2a3db892
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-12 12:55:44 +0530

    added login form

--- FrontEnd/src/components/Login.js
+++ FrontEnd/src/components/Login.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import loginService from '../services/login';
 
 const Login = () => {
-    return (
-        <div>
-            This is the login Page
-        </div>
-    )
-}
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Login
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+    } catch (error) {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
