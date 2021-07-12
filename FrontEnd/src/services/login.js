430935ceb93bcead2dc34122599e53fd36074d94
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-12 12:41:55 +0530

    added login service

--- FrontEnd/src/services/login.js
+++ FrontEnd/src/services/login.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 const baseUrl = 'http://localhost:3001/api/users';
 
-const signup = async (info) => {
-  const response = await axios.post(baseUrl, info);
+const login = async (credentials) => {
+  const response = await axios.post(baseUrl, credentials);
   return response.data;
 };
 
 // eslint-disable-next-line
-export default { signup };
+export default { login };
