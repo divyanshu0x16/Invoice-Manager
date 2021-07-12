8e2874f962aaf9468690412c7eea7e3a095b032f
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-12 23:44:21 +0530

    added dark mode utility

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,16 +4,19 @@ import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Navbar from './components/Navbar';
+import { ThemeProvider } from './utils/themeContext';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Dashboard} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-      </Switch>
+      <ThemeProvider>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
