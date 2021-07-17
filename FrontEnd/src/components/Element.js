ee95aa2302b4cda5fa100a7c3a160b20f5070334
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-18 02:45:11 +0530

    footer without bottom

--- FrontEnd/src/components/Element.js
+++ FrontEnd/src/components/Element.js
@@ -44,62 +44,80 @@ const Element = () => {
   });
 
   return (
-    <motion.div
-      className="mx-6 md:mx-auto"
-      initial={{ scale: 1.0, opacity: 0.5 }}
-      animate={{ scale: 1.0, opacity: 1 }}
-      transition={{ duration: 1 }}
-    >
-      <Link to={'/'}>
-        <div className="lg:pt-24 md:pt-16 pt-8 text-sm hover:">
-          <div className="flex">
-            <div className="self-center">
-              <ChevronLeftIcon className="text-all-bp h-8 w-8" />
-            </div>
-            <div className="self-center pt-1 transform hover:scale-105 duration-300">Go Back</div>
-          </div>
-        </div>
-      </Link>
-      <div className="pt-4">
-        <div className="grid grid-cols-2 bg-white dark:bg-navbar-darkbg py-4 rounded-lg">
-          <div className="self-center pl-8 font-bold">Status</div>
-          <div className="justify-self-end pr-8">
-            {invoice.type === 'paid' ? <Paid /> : <Pending />}
-          </div>
-        </div>
-      </div>
-      <div className="pt-4">
-        <div className="transform-colors duration-300 bg-white dark:bg-navbar-darkbg rounded-lg px-8 py-4">
-          <div className="text-xs font-normal">
-            <Description invoice={invoice} />
-          </div>
-          <div>
-            <div className="py-6 text-xs">
-              <Address invoice={invoice} />
-            </div>
-          </div>
-          <div className="grid grid-cols-2">
-            <div className="grid grid-rows-2">
-              <div>
-                <InvoiceDate invoice={invoice} />
+    <>
+      <motion.div
+        className="mx-6 md:mx-auto "
+        initial={{ scale: 1.0, opacity: 0.5 }}
+        animate={{ scale: 1.0, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <Link to={'/'}>
+          <div className="lg:pt-24 md:pt-16 pt-8 text-sm hover:">
+            <div className="flex">
+              <div className="self-center">
+                <ChevronLeftIcon className="text-all-bp h-8 w-8" />
               </div>
-              <div className="text-xs pt-3">
-                <PaymentDue invoice={invoice} />
+              <div className="self-center pt-1 transform hover:scale-105 duration-300">
+                Go Back
               </div>
             </div>
-            <div className="text-xs">
-              <BillTo invoice={invoice} />
+          </div>
+        </Link>
+        <div className="pt-4">
+          <div className="grid grid-cols-2 bg-white dark:bg-navbar-darkbg py-4 rounded-lg shadow-lg">
+            <div className="self-center pl-8 font-bold">Status</div>
+            <div className="justify-self-end pr-8">
+              {invoice.type === 'paid' ? <Paid /> : <Pending />}
             </div>
           </div>
-          <div className="text-xs py-8">
-            <SentTo invoice={invoice} />
-          </div>
-          <div className="pt-4">
-            <Item invoice={invoice} />
+        </div>
+        <div className="pt-4">
+          <div className="transform-colors duration-300 bg-white dark:bg-navbar-darkbg rounded-lg px-8 py-4 shadow-lg">
+            <div className="text-xs font-normal">
+              <Description invoice={invoice} />
+            </div>
+            <div>
+              <div className="py-6 text-xs">
+                <Address invoice={invoice} />
+              </div>
+            </div>
+            <div className="grid grid-cols-2">
+              <div className="grid grid-rows-2">
+                <div>
+                  <InvoiceDate invoice={invoice} />
+                </div>
+                <div className="text-xs pt-3">
+                  <PaymentDue invoice={invoice} />
+                </div>
+              </div>
+              <div className="text-xs">
+                <BillTo invoice={invoice} />
+              </div>
+            </div>
+            <div className="text-xs py-8">
+              <SentTo invoice={invoice} />
+            </div>
+            <div className="pt-4">
+              <Item invoice={invoice} />
+            </div>
           </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+      <footer className="transition-colors duration-300 bg-all-lightbg dark:bg-all-darkbg dark:text-white">
+        <div className="flex flex-row-reverse space-x-4 px-8 bg-white dark:bg-navbar-darkbg py-4 md:hidden">
+          <div className="cursor-pointer self-center bg-all-bp font-bold px-4 py-4 rounded-3xl text-white transform hover:scale-105 duration-300">
+            Mark As Paid
+          </div>
+          <div></div>
+          <div className="cursor-pointer self-center bg-red-700 font-bold px-4 py-4 rounded-3xl text-white transform hover:scale-105 duration-300">
+            Delete
+          </div>
+          <div className="cursor-pointer self-center bg-item-lightbg font-bold dark:bg-item-darkbg px-4 py-4 rounded-3xl transform hover:scale-105 duration-300">
+            Edit
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 
