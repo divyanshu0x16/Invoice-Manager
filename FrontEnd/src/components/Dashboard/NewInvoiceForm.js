9e662cf548aad98d6838069e602acad36f9e5f68
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-19 22:25:19 +0530

    initialized form with footer

--- FrontEnd/src/components/Dashboard/NewInvoiceForm.js
+++ FrontEnd/src/components/Dashboard/NewInvoiceForm.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 const Form = ({ setForm }) => {
+  
+  const inputClass =
+    'w-full h-10 transition-colors duration-300 pl-2 rounded-md dark:bg-item-darkbg dark:border-all-darkbg border-gray-300 border-2 focus:border-all-bp dark:focus:border-all-bp focus:outline-none';
+
   return (
     <div className="md:w-element flex flex-col justify-between min-h-screen">
       <div className="mx-8 mt-8 overflow-y-scroll">
         <div className="text-3xl font-bold">Create Invoice</div>
+        <section className="mt-6">
+          <div className="text-sm text-all-bp font-bold">Bill From</div>
+          <div className="mt-4">
+            <div className="text-xs">Street Address</div>
+            <div className="mt-3 mr-10">
+              <input className={inputClass} />
+            </div>
+          </div>
+        </section>
       </div>
       <div className="sticky mx-8 flex justify-between pb-8">
         <div
@@ -29,7 +42,7 @@ const NewInvoiceForm = ({ form, setForm }) => {
   return (
     <div className={form}>
       <div className="flex mt-16 md:mt-0">
-        <div className="flex-grow md:flex-grow-0 transition-colors duration-300 rounded-r-2xl md:ml-24 bg-white dark:bg-all-darkbg dark:text-white w-max min-h-screen">
+        <div className="flex-grow md:flex-grow-0 transition-colors duration-300 rounded-r-2xl md:ml-24 bg-all-lightbg dark:bg-all-darkbg dark:text-white w-max min-h-screen">
           <Form setForm={setForm} />
         </div>
         <div
