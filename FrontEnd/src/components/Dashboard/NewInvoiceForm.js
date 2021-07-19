1227dbcb0f667d25c17efb668e09df1d8319dac9
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-19 19:34:00 +0530

    fixed form height

--- FrontEnd/src/components/Dashboard/NewInvoiceForm.js
+++ FrontEnd/src/components/Dashboard/NewInvoiceForm.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+const Form = () => {
+  return <div className="md:w-element">Form Goes Here</div>;
+};
+
 const NewInvoiceForm = ({ form, setForm }) => {
   return (
     <div className={form}>
       <div className="flex mt-16 md:mt-0">
-        <div className="transition-colors duration-300 rounded-r-2xl md:ml-24 bg-white dark:bg-all-darkbg dark:text-white w-max min-h-screen">
-          Form Goes Here
+        <div className="flex-grow md:flex-grow-0 transition-colors duration-300 rounded-r-2xl md:ml-24 bg-white dark:bg-all-darkbg dark:text-white w-max min-h-screen">
+          <Form />
         </div>
         <div
           onClick={() =>
@@ -13,7 +17,7 @@ const NewInvoiceForm = ({ form, setForm }) => {
               'z-10 max-w-full absolute inset-y-0 w-screen transform -translate-x-full transition duration-300 ease-in-out'
             )
           }
-          className="flex-grow backdrop-filter backdrop-brightness-50"
+          className="hidden md:block flex-grow backdrop-filter backdrop-brightness-50"
         ></div>
       </div>
     </div>
